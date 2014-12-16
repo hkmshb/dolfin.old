4b83a6aa48cf557ae1256b88cba08fa2c46543cc
hkmshb <hkmshb@gmail.com>
2014-12-16 17:15:38 +0100

    Added classes for structured handling of parsed cli arguments

--- dolfin.py
+++ dolfin.py
@@ -13,10 +13,17 @@ __version__ = '0.1'
 import os, sys
 import json
 
+from abc import ABCMeta, abstractmethod
+
 
 
 class DolfinError(Exception):
     """Represents the base exception class for the library."""
+    pass
+
+
+class CommandError(DolfinError):
+    """The exception thrown for a command related error."""
     pass
 
 
@@ -108,6 +115,72 @@ def include_revision(func):
     f.__module__ = func.__module__
     f.__dict__.update(func.__dict__)    
     return f
+
+
+class Command(object):
+    """Represents the base class for Command objects."""
+    __metaclass__ = ABCMeta
+
+    def _execute(self, args):
+        """Tries to execute this command; if it raises a CommandError, intercept
+        it and print it sensibly to stderr.
+        """
+        try:
+            output = self._handle(args)
+            if output: print(output)
+        except CommandError as ex:
+            sys.stderr.write('Error: %s\n\n' % str(ex))
+            sys.exit(1)
+    
+    def print_help(self, prog):
+        """Prints the help message for this command."""
+        p = self.create_parser(prog)
+        p.print_help()
+        print('')
+
+    def run_from_argv(self, argv=None):
+        """Handles default options then runs this command."""
+        print('')
+        argv = (sys.argv[1:] if argv is None else argv)
+        p = self._create_parser()
+        args = p.parse_args(argv)
+
+        self._handle_default_args(args)
+        self._execute(args)
+    
+    def _handle_default_args(self, args):
+        if hasattr(args, 'pythonpath'):
+            sys.path.insert(0, args.pythonpath)
+
+    @abstractmethod
+    def _create_parser(self):
+        """Creates and returns the ArgumentParser which will be used to parse
+        the arguments passed to this command. Must be overridden by subclasses.
+        """
+        pass
+    
+    @abstractmethod
+    def _handle(self, args):
+        """Performs the actual operation of the command. Must be overridden
+        by subclasses.
+        """
+        pass
+
+
+class SubCommand(object):
+    """Represents the base class for a SubCommand object."""
+    __metaclass__ = ABCMeta
+
+    def __init__(self, parent):
+        self._subparser = parent.create_subparser(self)
+        self._parent = parent
+    
+    def _error(self, message):
+        self._subparser.error(message)
+
+    @abstractmethod
+    def __call__(self, args):
+        pass
 
 
 class Storage(dict):
--- tests/tests_dolfin.py
+++ tests/tests_dolfin.py
@@ -289,3 +289,55 @@ class ConfigTest(unittest.TestCase):
         );
         self.assertEqual('factory-location', conf.factory)
 
+
+class FakeCommand(dolfin.Command):
+    
+    prog = 'fake'
+    desc = 'This is a fake implementation of the Command class'
+
+    def __init__(self):
+        self.p = None
+        self.register = set()
+
+    def _execute(self, args):
+        self.register.add('_execute')
+        return super(FakeCommand, self)._execute(args)
+        
+    def _create_parser(self):
+        if self.p == None:
+            from argparse import ArgumentParser
+            
+            p = ArgumentParser(prog=self.prog, description=self.desc)
+            add = lambda *a, **k: p.add_argument(*a, **k)
+            add('-p', '--port', type=int, default=None, help='server port')
+            add('--host', help='server hostname')
+            add('-s', '--show', action='store_true', help='show server details')
+            self.register.add('_create_parser')
+            self.p = p
+        return self.p
+    
+    def _handle(self, args):
+        self.register.add('_handle')
+        if args.show:
+            return "server='%s:%s'" % (args.port or 80, args.host or 'localhost')
+
+
+class CommandTest(unittest.TestCase):
+    
+    def test_cannot_create_abstract_command(self):
+        self.assertRaises(TypeError, dolfin.Command)
+
+    def test_cannot_create_abstract_subcommand(self):
+        self.assertRaises(TypeError, dolfin.SubCommand)
+
+    def test_are_best_invoked_using_run_from_argv(self):
+        cmd = FakeCommand()
+        self.assertEqual(0, len(cmd.register))
+        cmd.run_from_argv(['-p', '8080', '-s'])
+
+        self.assertEqual(3, len(cmd.register))
+        self.assertTrue(
+            '_execute' in cmd.register and
+            '_create_parser' in cmd.register and
+            '_handle' in cmd.register
+        )
